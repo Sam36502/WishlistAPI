f2d0b47439df3001833e77060749206817cf1f90
Bismarck <sam@aepearce.com>
2022-05-19 01:44:20 +0200

    Testing reading certificates

--- src/main.go
+++ src/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,6 +14,12 @@ func main() {
 	// Initialization
 	ConnectDB()
 	InitRoutes(e)
+
+	f, err := ioutil.ReadFile("/certs/fullchain.pem")
+	if err != nil {
+		fmt.Printf("Error: Failed to open file:\n  %s\n", err)
+	}
+	fmt.Println(string(f))
 
 	e.Logger.Fatal(e.StartTLS(":"+os.Getenv("WISHLIST_API_PORTNUM"), os.Getenv("WISHLIST_SSL_CERT"), os.Getenv("WISHLIST_SSL_KEY")))
 	//e.Logger.Fatal(e.Start(":2512"))
